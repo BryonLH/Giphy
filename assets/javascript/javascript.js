23871bc79b63d0e46952578e84929a719fde92ea
BryonLH <bryonlh@gmail.com>
2019-06-30 13:12:45 -0700

    created function to get the gifs

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,53 +1,61 @@
-var giphys = ["Micky Mouse", "Donald Duck", "Roger Rabit", "Goofy", "Minnie Mouse"];
+var gifArray = ["Micky Mouse", "Donald Duck", "Roger Rabit", "Goofy", "Minnie Mouse"];
 var giphyAPIKey = "QaQRN9wZhZfIxKBf1DxDasjnsXjgCoqS";
-var giphyURL = "https://api.giphy.com/v1/gifs/search?api_key=" + giphyAPIKey + "&q=" + searchQuery + "&limit=15&offset=0&rating=PG&lang=en";
+// var giphyURL = "https://api.giphy.com/v1/gifs/search?api_key=" + giphyAPIKey + "&q=" + searchQuery + "&limit=10&offset=0&rating=PG&lang=en";
 var searchQuery = "Donald Duck";
 
-$.ajax({
-    url: giphyURL,
-    method: "GET"
-  }).then(function(response) {
-      console.log(response);
-  });
+
+
 
 function renderButtons() {
-    for (var i = 0; i < giphys.length; i++) {
+    for (var i = 0; i < gifArray.length; i++) {
         var a = $("<button>");
-        a.addClass("gif");
-        a.attr("data-name", giphys[i]);
-        a.text(giphys[i]);
+        a.addClass("gif-button");
+        a.attr("data-character", gifArray[i]);
+        a.text(gifArray[i]);
         $("#buttons").append(a);
-      }
     }
+}
 
-    // This function handles events where one button is clicked
-    $("#add-movie").on("click", function(event) {
-      event.preventDefault();
 
-      var gif = $("#giphys")
-        .val()
-        .trim();
+// This function handles events where one button is clicked
+$("#submit").on("click", function (event) {
+    event.preventDefault();
+    var gif = $("#giphy-search").val().trim();
+    gifArray.push(gif);
+    $("#buttons").empty();
+    renderButtons();
+});
 
-      giphys.push(gif);
 
-      // Calling renderButtons which handles the processing of our movie array
-      renderButtons();
+
+renderButtons();
+
+$("button").on("click", function () {
+    var character = $(this).attr("data-character");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + giphyAPIKey + "&q=" + character + "+disney&limit=10&lang=en";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        console.log(response.data);
+        var results = response.data;
+        // for (var i = 0;i < response.length; i++) {
+        results.forEach(function (gif) {
+            if (gif.rating !== "r") {
+                var gifDiv = $("<div class='gif-div'>");
+                var rating = gif.rating;
+                console.log(rating);
+                var p = $("<p>").text("Rating: " + rating);
+                var characterImage = $("<img>");
+                characterImage.attr("src", gif.images.fixed_height.url);
+                gifDiv.prepend(p);
+                gifDiv.prepend(characterImage);
+                $("#gif-section").prepend(gifDiv);
+            }
+        })
+
+
+
     });
-
-
-    $("#submit").on("click", function(event) {
-        event.preventDefault();
-
-        // This line grabs the input from the textbox
-        var gif = $("#giphy-search")
-          .val()
-          .trim();
-
-        // The movie from the textbox is then added to our array
-        giphys.push(gif);
-
-        // Calling renderButtons which handles the processing of our movie array
-        renderButtons();
-      });
-
-      renderButtons();
+})
